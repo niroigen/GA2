033f9f7653f1cb295c30cf4fadf3c87330f05096
Niroigen Suntharam <niroigen@gmail.com>
2020-03-22 01:32:29 -0400

    able to extract population and save it to a function

--- jsonReader.cpp
+++ jsonReader.cpp
@@ -9,9 +9,11 @@
 
 using namespace std;
 
+const int NUM_INDIVIDUALS = 100;
+
 Individual* createBaseIndividual(int numTiles, const Json::Value &obj);
 
-int main() {
+void initializePopulation(Individual **population) {
     ifstream ifs("population.json");
     Json::Reader reader;
     Json::Value obj;
@@ -21,11 +23,7 @@ int main() {
 
     Individual *baseIndividual = createBaseIndividual(numTiles, obj);
 
-    const int NUM_INDIVIDUALS = 100;
-
-    Individual *pop[NUM_INDIVIDUALS];
-
-    pop[0] = baseIndividual;
+    population[0] = baseIndividual;
 
     for (int i = 1; i < NUM_INDIVIDUALS; i++)
     {
@@ -33,26 +31,25 @@ int main() {
 
         for (int j = 0; j < numTiles; j++)
         {
-            unsigned int x = obj[to_string(j)]["puzzle"][j][0].asInt();
-            unsigned int y = obj[to_string(j)]["puzzle"][j][1].asInt();
-            unsigned int l = obj[to_string(j)]["puzzle"][j][2].asInt();
-            unsigned int w = obj[to_string(j)]["puzzle"][j][3].asInt();
-
-            tilesTest[j].x = x;
-            tilesTest[j].y = y;
-            tilesTest[j].l = l;
-            tilesTest[j].w = w;
+            tilesTest[j].x = obj[to_string(j)]["puzzle"][j][0].asInt();
+            tilesTest[j].y = obj[to_string(j)]["puzzle"][j][1].asInt();
+            tilesTest[j].l = obj[to_string(j)]["puzzle"][j][2].asInt();
+            tilesTest[j].w = obj[to_string(j)]["puzzle"][j][3].asInt();
         }
 
-        pop[i] = new Individual(tilesTest, numTiles, baseIndividual);
+        population[i] = new Individual(tilesTest, numTiles, baseIndividual);
     }
+}
+
+int main()
+{
+    Individual *population[NUM_INDIVIDUALS];
+    initializePopulation(population);
 
     for (int i = 0; i < NUM_INDIVIDUALS; i++)
     {
-        delete pop[i];
+        delete population[i];
     }
-
-    return 0;
 }
 
 Individual* createBaseIndividual(int numTiles, const Json::Value &obj)
@@ -61,15 +58,10 @@ Individual* createBaseIndividual(int numTiles, const Json::Value &obj)
 
     for (int i = 0; i < numTiles; i++)
     {
-        unsigned int x = obj["0"]["puzzle"][i][0].asInt();
-        unsigned int y = obj["0"]["puzzle"][i][1].asInt();
-        unsigned int l = obj["0"]["puzzle"][i][2].asInt();
-        unsigned int w = obj["0"]["puzzle"][i][3].asInt();
-
-        tiles[i].x = x;
-        tiles[i].y = y;
-        tiles[i].l = l;
-        tiles[i].w = w;
+        tiles[i].x = obj["0"]["puzzle"][i][0].asInt();
+        tiles[i].y = obj["0"]["puzzle"][i][1].asInt();
+        tiles[i].l = obj["0"]["puzzle"][i][2].asInt();
+        tiles[i].w = obj["0"]["puzzle"][i][3].asInt();
     }
 
     return new Individual(tiles, numTiles);
